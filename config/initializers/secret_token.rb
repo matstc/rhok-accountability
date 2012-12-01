65566b6626d2bc3ad1275cd13fe402710cd33c04
Matt <matt@matthias.(none)>
2012-12-01 15:29:00 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RhokACCOUNTability::Application.config.secret_token = '306978e6c45c63315e48d63808c4d815a441a4f2d0ea439dd8de6d1aa24cf3019f221ec4014a3704ffe7ea0b734cac2f0a7b0e4a690a71955e4c5866f8203afc'
