c857b0ed3da7581d48ffae3240647856bb042988
Matt <matt@matthias.(none)>
2012-12-02 12:37:06 +0100

    added a view to /add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,10 +29,11 @@ class ApplicationController < ActionController::Base
   def add
     session = GoogleDrive.login("rhokingout@gmail.com", "accountability1")
     ws_number_to_key = session.spreadsheet_by_key("0AsNrDUUNJ35MdFJkOUZZaTNzeTdPQTRWNmV2ZzJydFE").worksheets[0]
-    hash_row = ws_number_to_key.list.to_hash_array.find{|list_row| list_row["Phone Number"] == params[:From]}
+    rows = ws_number_to_key.list.to_hash_array
+    hash_row = rows.find{|list_row| list_row["Phone Number"] == params[:From]}
         
     ws_account = session.spreadsheet_by_key(hash_row["Spreadsheet Key"]).worksheets[0]
-    values = params[Body].split(",").map{|value| value.strip}
+    values = params[:Body].split(",").map{|value| value.strip}
         
     ws_account.list.push({"timestamp" => Time.now, 
                            "phone number" => params[:From], 
