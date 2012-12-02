54b1d0c253ff4b3adf825633951b4573e9746780
Sebastian Kießling <sebkies@gmail.com>
2012-12-02 18:08:37 +0100

    fixing errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   
   def create
     session = GoogleDrive.login(ENV['gmail'], ENV['gmailp'])
-<<<<<<< HEAD
+
     file = session.upload_from_file("config/initializers/ACCOUNTability.xlsx","ACCOUNTability: "+params[:project_name], :content_type => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
     spreadsheet = session.spreadsheet_by_url(file.human_url)
     spreadsheet.worksheets.each {|ws|
@@ -19,27 +19,14 @@ class ApplicationController < ActionController::Base
       ws.max_rows = 200
       ws.save
     }
-    ws_number_to_key = session.spreadsheet_by_key(ENV['spreadsheet_key']).worksheets[0]
-    ws_number_to_key.list.push({"Phone Number" => params[:phone_number], 
-                       "Spreadsheet Key" => spreadsheet.key})
-=======
-    spreadsheet = session.create_spreadsheet("ACCOUNTability: "+params[:project_name])
-    ws_dashboard = spreadsheet.worksheets[0]
-    ws_dashboard.title = "Dashboard"
-    ws_dashboard[1,1] = "Hello"
-    ws_dashboard.save
-    
-    ws_generated = spreadsheet.add_worksheet("data")
-    ws_generated.list.keys = ["timestamp", "phone number", "item", "description", "amount"]
-                       
-    ws_generated.save
+
+
     
     number_to_key = session.spreadsheet_by_title(@@DATABASE_SPREADSHEET_TITLE)
     number_to_key = create_spreadsheet_database(session) if number_to_key.blank?
     
     ws_number_to_key = number_to_key.worksheets[0]
     ws_number_to_key.list.push({"Phone Number" => params[:phone_number], "Spreadsheet Key" => spreadsheet.key})
->>>>>>> 5423d2fcdc61cafbe6308e55199c14d80129ebf2
     ws_number_to_key.save
     spreadsheet.acl.push({:scope_type => "user", :scope => params[:email], :role => "owner"})
     flash[:notice] = "Your new spreadsheet is available <a href='#{spreadsheet.human_url}'> here.</a>"
@@ -52,7 +39,6 @@ class ApplicationController < ActionController::Base
     ws[1,1] = "Phone Number"
     ws[1,2] = "Spreadsheet Key"
     ws.save
-
     spreadsheet
   end
 
