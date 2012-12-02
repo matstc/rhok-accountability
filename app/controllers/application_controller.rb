650cc68391876075207bec3fc20b21a696f73ebf
Matt <matt@matthias.(none)>
2012-12-02 16:30:32 +0100

    fixed it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,6 +30,7 @@ class ApplicationController < ActionController::Base
     session = GoogleDrive.login(ENV['gmail'], ENV['gmailp'])
     ws_number_to_key = session.spreadsheet_by_key(ENV['spreadsheet_key']).worksheets[0]
     hash_row = ws_number_to_key.list.to_hash_array.find{|list_row| list_row["Phone Number"] == params[:From]}
+    p "hash row = #{hash_row}"
         
     ws_account = session.spreadsheet_by_key(hash_row["Spreadsheet Key"]).worksheet_by_title("data")
     values = params[:Body].split(",").map{|value| value.strip}
